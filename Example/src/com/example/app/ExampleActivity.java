ecafc4754261ce2c12d4add58e31e78305a2699e
Andrew Beale <andrew.beale@meetmoi.com>
2015-03-13 14:02:38 -0400

    Added ability to build as AAR library using Gradle

--- Example/src/com/example/app/ExampleActivity.java
+++ Example/src/com/example/app/ExampleActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import com.indicative.client.java.android.Indicative;
+import com.indicative.client.android.Indicative;
 
 import java.util.HashMap;
 import java.util.Map;
